3c71f1e48ccf17ad191ef77518102e24ff4d5139
Alceujrab <alceujr.ab@gmail.com>
2026-03-19 09:22:27 -0300

    fix(crm): add missing task routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\InboxController;
 use App\Http\Controllers\CrmController;
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,6 +29,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/crm', [CrmController::class, 'index'])->name('crm.index');
     Route::put('/crm/deals/{deal}/move', [CrmController::class, 'moveDeal'])->name('crm.deals.move');
     Route::get('/api/crm/deals/{deal}', [CrmController::class, 'show'])->name('api.crm.deals.show');
+    Route::post('/api/crm/deals/{deal}/tasks', [TaskController::class, 'store'])->name('api.tasks.store');
+    Route::put('/api/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('api.tasks.toggle');
+    Route::delete('/api/tasks/{task}', [TaskController::class, 'destroy'])->name('api.tasks.destroy');
     
     Route::get('/catalog', [VehicleController::class, 'index'])->name('catalog.index');
 
